a843ecde9cd1082ef8311a47cbe86edec2d28ecc
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-09-04 21:51:45 -0500

    added edit and delete functionality

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,31 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    const editTitle = document.querySelector('input[name="post-title"]').value;
+
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title: editTitle
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    // alert(response);
+
+    // console.log(response);
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
+
+}
+  
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
